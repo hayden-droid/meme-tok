5abc4ad860f9b9d2dcf9001d4c89498da4389c52
Descendo The Monkei <61548447+hayden-droid@users.noreply.github.com>
2023-09-30 14:10:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,59 +4,72 @@ document.addEventListener("DOMContentLoaded", function () {
     const nextButton = document.getElementById("next-button");
     const progressBar = document.getElementById("progress-bar");
     const playButton = document.getElementById("play-button");
-    const videoSources = [
-        "videos/video1.mp4",
-        "videos/video2.mp4",
-        "videos/video3.mp4",
-        "videos/video4.mp4",
-    ];
-    let currentVideoIndex = 0;
 
-    function loadVideo(index) {
-        if (index >= 0 && index < videoSources.length) {
-            videoPlayer.src = videoSources[index];
-            currentVideoIndex = index;
-        }
-    }
+    // Initialize an empty array for video sources
+    let videoSources = [];
 
-    prevButton.addEventListener("click", function () {
-        loadVideo(currentVideoIndex - 1);
-    });
+    // Fetch video data from videos2.json
+    fetch('videos2.json')
+        .then((response) => response.json())
+        .then((data) => {
+            // Extract "VideoDirectURL" directly from each data object and push it to videoSources
+            data.forEach((videoData) => {
+                const videoDirectURL = videoData.VideoDirectURL;
+                videoSources.push(videoDirectURL);
+            });
 
-    nextButton.addEventListener("click", function () {
-        loadVideo(currentVideoIndex + 1);
-    });
+            // Initialize the current video index
+            let currentVideoIndex = 0;
 
-    videoPlayer.addEventListener("timeupdate", updateProgressBar);
+            function loadVideo(index) {
+                if (index >= 0 && index < videoSources.length) {
+                    videoPlayer.src = videoSources[index];
+                    currentVideoIndex = index;
+                }
+            }
 
-    function updateProgressBar() {
-        const currentTime = videoPlayer.currentTime;
-        const duration = videoPlayer.duration;
-        const percentage = (currentTime / duration) * 100;
-        progressBar.style.width = percentage + "%";
-    }
+            prevButton.addEventListener("click", function () {
+                loadVideo(currentVideoIndex - 1);
+            });
 
-    function togglePlay() {
-        if (videoPlayer.paused || videoPlayer.ended) {
-            videoPlayer.play();
-            playButton.innerHTML = '<img src="icons/pause.svg" alt="Pause">';
-        } else {
-            videoPlayer.pause();
-            playButton.innerHTML = '<img src="icons/play.svg" alt="Play">';
-        }
-    }
+            nextButton.addEventListener("click", function () {
+                loadVideo(currentVideoIndex + 1);
+            });
 
-    playButton.addEventListener("click", togglePlay);
+            videoPlayer.addEventListener("timeupdate", updateProgressBar);
 
-    // Handle arrow key navigation
-    document.addEventListener("keydown", function (event) {
-        if (event.key === "ArrowUp") {
-            loadVideo(currentVideoIndex - 1);
-        } else if (event.key === "ArrowDown") {
-            loadVideo(currentVideoIndex + 1);
-        }
-    });
+            function updateProgressBar() {
+                const currentTime = videoPlayer.currentTime;
+                const duration = videoPlayer.duration;
+                const percentage = (currentTime / duration) * 100;
+                progressBar.style.width = percentage + "%";
+            }
 
-    // Load the first video
-    loadVideo(currentVideoIndex);
+            function togglePlay() {
+                if (videoPlayer.paused || videoPlayer.ended) {
+                    videoPlayer.play();
+                    playButton.innerHTML = '<img src="icons/pause.svg" alt="Pause">';
+                } else {
+                    videoPlayer.pause();
+                    playButton.innerHTML = '<img src="icons/play.svg" alt="Play">';
+                }
+            }
+
+            playButton.addEventListener("click", togglePlay);
+
+            // Handle arrow key navigation
+            document.addEventListener("keydown", function (event) {
+                if (event.key === "ArrowUp") {
+                    loadVideo(currentVideoIndex - 1);
+                } else if (event.key === "ArrowDown") {
+                    loadVideo(currentVideoIndex + 1);
+                }
+            });
+
+            // Load the first video
+            loadVideo(currentVideoIndex);
+        })
+        .catch((error) => {
+            console.error('Error fetching video data:', error);
+        });
 });
